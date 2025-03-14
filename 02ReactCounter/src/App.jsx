d302221217147a3d890f6898fc8e837d0037f9ce
codingashishdev <atankmember@gmail.com>
2025-03-14 20:32:41 +0530

    add initial React counter app with Vite configuration and basic components

--- 02ReactCounter/src/App.jsx
+++ 02ReactCounter/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 
 function App() {
-    const [counter, setCounter] = useState(0);
+    let [counter, setCounter] = useState(0);
 
     const add = () => {
         setCounter(counter+1)
     };
 
     const remove = () => {
+        if(counter-1 < 0)return;
         setCounter(counter-1)
     };
 
@@ -16,6 +17,7 @@ function App() {
             <h1>React Counter</h1>
             <h2>Counter value: {counter} </h2>
 
+            {/* don't do add() or remove() function will be executed without waiting for a click */}
             <button onClick={add}>Add</button>
             <button onClick={remove}>Descrease</button>
         </>
