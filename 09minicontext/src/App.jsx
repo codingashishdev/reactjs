3beb28a7d9e7dc7fa33e7bf4855b7779ad770bfc
Ashish Prajapati <108178021+codingashishdev@users.noreply.github.com>
2025-03-26 17:37:09 +0530

    initialize React app with Vite; add UserContext and Login component; set up basic styling and configuration

---
+++ 09minicontext/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import UserContectProvider from "./context/UserContectProvider";
+
+function App() {
+    return (
+        <UserContectProvider>
+            
+        </UserContectProvider>
+    );
+}
+
+export default App;
